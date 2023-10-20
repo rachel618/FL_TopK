cbc05cc5d0c993db24b5b0e889e5591243556c98
rachel618 <jungjihyun8762@gmail.com>
2023-10-20 13:31:02 +0900

    FIX: update 2kchunk

--- dataset/generate_mnist.py
+++ dataset/generate_mnist.py
@@ -2,45 +2,34 @@ import numpy as np
 import os
 import sys
 import random
+import argparse
 import torch
 import torchvision
 import torchvision.transforms as transforms
 from utils.dataset_utils import check, separate_data, split_data, save_file
 
-
-random.seed(1)
-np.random.seed(1)
-num_clients = 20
-num_classes = 10
-dir_path = "mnist/"
-
-
+import os
+DATA_PATH = os.path.dirname(os.path.abspath(__file__)) 
 # Allocate data to users
-def generate_mnist(dir_path, num_clients, num_classes, niid, balance, partition):
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+def generate_mnist(data_path, save_path,  num_clients, num_classes, niid, balance, partition):
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
         
     # Setup directory for train/test data
-    config_path = dir_path + "config.json"
-    train_path = dir_path + "train/"
-    test_path = dir_path + "test/"
-
+    config_path = os.path.join(save_path, 'config.json')
+    train_path = os.path.join(save_path, 'train')
+    test_path = os.path.join(save_path, 'test')
+    
     if check(config_path, train_path, test_path, num_clients, num_classes, niid, balance, partition):
         return
-
-    # FIX HTTP Error 403: Forbidden
-    from six.moves import urllib
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-    urllib.request.install_opener(opener)
 
     # Get MNIST data
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
 
     trainset = torchvision.datasets.MNIST(
-        root=dir_path+"rawdata", train=True, download=True, transform=transform)
+        root=os.path.join(data_path,"rawdata"), train=True, download=True, transform=transform)
     testset = torchvision.datasets.MNIST(
-        root=dir_path+"rawdata", train=False, download=True, transform=transform)
+        root=os.path.join(data_path,"rawdata"), train=False, download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(
         trainset, batch_size=len(trainset.data), shuffle=False)
     testloader = torch.utils.data.DataLoader(
@@ -54,10 +43,11 @@ def generate_mnist(dir_path, num_clients, num_classes, niid, balance, partition)
     dataset_image = []
     dataset_label = []
 
-    dataset_image.extend(trainset.data.cpu().detach().numpy())
-    dataset_image.extend(testset.data.cpu().detach().numpy())
+    dataset_image.extend(trainset.data.repeat(1,3,1,1).cpu().detach().numpy())
+    dataset_image.extend(testset.data.repeat(1,3,1,1).cpu().detach().numpy())
     dataset_label.extend(trainset.targets.cpu().detach().numpy())
     dataset_label.extend(testset.targets.cpu().detach().numpy())
+    print(len(dataset_image), len(dataset_label))
     dataset_image = np.array(dataset_image)
     dataset_label = np.array(dataset_label)
 
@@ -74,8 +64,21 @@ def generate_mnist(dir_path, num_clients, num_classes, niid, balance, partition)
 
 
 if __name__ == "__main__":
-    niid = True if sys.argv[1] == "noniid" else False
-    balance = True if sys.argv[2] == "balance" else False
-    partition = sys.argv[3] if sys.argv[3] != "-" else None
-
-    generate_mnist(dir_path, num_clients, num_classes, niid, balance, partition)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--niid', type=bool, default=False)
+    parser.add_argument('--balance', type=bool, default=True)
+    parser.add_argument('--partition', type=bool, default=None)
+    parser.add_argument('--num_clients', type=int, default=100)
+    parser.add_argument('--dataset', type=str, default='mnist')
+    parser.add_argument('--save_path', type=str, default='mnist')
+    args = parser.parse_args()
+    
+    random.seed(1)
+    np.random.seed(1)
+    num_classes = 10
+   
+    data_path = os.path.join(DATA_PATH, args.dataset)
+    save_path = os.path.join(DATA_PATH, args.save_path)
+    
+    generate_mnist(data_path, save_path, args.num_clients, num_classes, args.niid, args.balance, args.partition)
